45712a695a0051b297f259fdfb60b41335d05a0e
Happy-Singh-Rajpurohit <happyrajpurohit2006@gmail.com>
2025-08-05 19:55:41 +0530

    nav-trial-2

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -7,8 +7,8 @@ export default function Navbar(){
         <nav className='navbar-container'>
             {/* Left Navbar Tags */}
             <div className='left-panel'>
-                <Link to="about" smooth={true} duration={500}>About</Link>
-                <Link to="tech" smooth={true} duration={500}>Tech</Link>
+                <Link to="About" smooth={true} duration={500}>About</Link>
+                <Link to="Tech" smooth={true} duration={500}>Tech</Link>
             </div>
 
             {/* Center Logo */}
@@ -18,8 +18,8 @@ export default function Navbar(){
 
             {/* Right Navbar Tags */}
             <div className='right-panel'>
-                <Link to="projects" smooth={true} duration={500}>Projects</Link>
-                <Link to="contact" smooth={true} duration={500}>Contact</Link>
+                <Link to="Projects" smooth={true} duration={500}>Projects</Link>
+                <Link to="Contact" smooth={true} duration={500}>Contact</Link>
             </div>
         </nav>
     )
